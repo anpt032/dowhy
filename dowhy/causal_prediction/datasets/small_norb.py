366193ae8b6ca486d3d70bfdbc1d64caea52a787
andyjsw <91446731+andyjsw@users.noreply.github.com>
2024-05-06 17:03:40 +0700

    Changes to be committed: 	modified:   dowhy/causal_prediction/datasets/small_norb.py

--- dowhy/causal_prediction/datasets/small_norb.py
+++ dowhy/causal_prediction/datasets/small_norb.py
@@ -223,10 +223,6 @@ class SmallNorbCausalAttribute(MultipleDomainDataset):
 
         images = torch.stack([images, images, images, images, images], dim=1)
 
-        print(len(images))
-        print(len(torch.tensor(range(len(images)))))
-        print(torch.tensor(range(len(images))))
-
         images[torch.tensor(range(len(images))), (4 - lightings).long(), :, :] *= 0
 
         x = images.float().div_(255.0)
