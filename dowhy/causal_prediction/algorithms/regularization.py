4a5573b3ce38f40a90a0c1f0314e38ea28010418
andyjsw <91446731+andyjsw@users.noreply.github.com>
2024-05-15 17:31:32 +0700

    Changes to be committed: 	modified:   dowhy/causal_prediction/algorithms/regularization.py

--- dowhy/causal_prediction/algorithms/regularization.py
+++ dowhy/causal_prediction/algorithms/regularization.py
@@ -144,9 +144,9 @@ class Regularizer:
                     cumprod = torch.cumprod(cardinality, dim=0)
                     n_groups = cumprod[-1].item()
 
-                    # factors_np = np.concatenate(([1], cumprod[:-1]))
-                    # factors = torch.from_numpy(factors_np)
-                    factors = torch.cat((torch.tensor([1]).cuda(), cumprod[:-1]))
+                    factors_np = np.concatenate(([1], cumprod[:-1]).cpu())
+                    factors = torch.from_numpy(factors_np)
+                    # factors = torch.cat((torch.tensor([1]).cuda(), cumprod[:-1]))
 
                     group_indices = grouping_data @ factors
 
@@ -210,9 +210,9 @@ class Regularizer:
                     cumprod = torch.cumprod(cardinality, dim=0)
                     n_groups = cumprod[-1].item()
 
-                    # factors_np = np.concatenate(([1], cumprod[:-1]))
-                    # factors = torch.from_numpy(factors_np)
-                    factors = torch.cat((torch.tensor([1]).cuda(), cumprod[:-1]))
+                    factors_np = np.concatenate(([1], cumprod[:-1]).cpu())
+                    factors = torch.from_numpy(factors_np)
+                    # factors = torch.cat((torch.tensor([1]).cuda(), cumprod[:-1]))
 
                     group_indices = grouping_data @ factors
 
@@ -253,9 +253,9 @@ class Regularizer:
                     cumprod = torch.cumprod(cardinality, dim=0)
                     n_groups = cumprod[-1].item()
 
-                    # factors_np = np.concatenate(([1], cumprod[:-1]))
-                    # factors = torch.from_numpy(factors_np)
-                    factors = torch.cat((torch.tensor([1]).cuda(), cumprod[:-1]))
+                    factors_np = np.concatenate(([1], cumprod[:-1]).cpu())
+                    factors = torch.from_numpy(factors_np)
+                    # factors = torch.cat((torch.tensor([1]).cuda(), cumprod[:-1]))
 
                     group_indices = grouping_data @ factors
 
