2d418d6fe454a18742a3ae288ab356f43aff0adc
andyjsw <91446731+andyjsw@users.noreply.github.com>
2024-05-22 10:10:50 +0700

    Changes to be committed: 	modified:   dowhy/causal_prediction_selfcode/algorithms/base_algorithm.py 	new file:   dowhy/causal_prediction_selfcode/algorithms/cacm.py 	new file:   dowhy/causal_prediction_selfcode/algorithms/erm.py 	new file:   dowhy/causal_prediction_selfcode/algorithms/regularization.py 	new file:   dowhy/causal_prediction_selfcode/dataloaders/__init__.py 	new file:   dowhy/causal_prediction_selfcode/dataloaders/fast_data_loader.py 	new file:   dowhy/causal_prediction_selfcode/dataloaders/get_data_loader.py 	new file:   dowhy/causal_prediction_selfcode/dataloaders/misc.py 	modified:   dowhy/causal_prediction_selfcode/datasets/base_dataset.py 	modified:   dowhy/causal_prediction_selfcode/datasets/mnist.py 	new file:   dowhy/causal_prediction_selfcode/datasets/small_norb.py 	new file:   dowhy/causal_prediction_selfcode/datasets/test.ipynb 	modified:   dowhy/causal_prediction_selfcode/models/networks.py 	deleted:    dowhy/causal_prediction_selfcode/regularization.py

--- dowhy/causal_prediction_selfcode/algorithms/base_algorithm.py
+++ dowhy/causal_prediction_selfcode/algorithms/base_algorithm.py
@@ -4,9 +4,18 @@ from torch.nn import functional as F
 
 
 class PredictionAlgorithm(pl.LightningModule):
-
     def __init__(self, model, optimizer, lr, weight_decay, betas, momentum):
+        """
+        This class implements the default methods for a Pytorch lightning module `pl.LightningModule`.
+        Its methods are called when the `fit()` method is called. To know more about these methods, refer to https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html.
 
+        :param model: Neural network modules used for training
+        :param optimizer: Optimization algorithm used for training. Currently supports "Adam" and "SGD".
+        :param lr: Value of learning rate
+        :param weight_decay: Value of weight decay for optimizer
+        :param betas: Adam configuration parameters (beta1, beta2), exponential decay rate for the first moment and second-moment estimates, respectively.
+        :param momentum: Value of momentum for SGD optimzer
+        """
         super().__init__()
 
         self.model = model
@@ -20,18 +29,28 @@ class PredictionAlgorithm(pl.LightningModule):
         if self.optimizer not in ["Adam", "SGD"]:
             error_msg = self.optimizer + " is not implemented currently. Try Adam or SGD."
             raise Exception(error_msg)
-        
+
     def training_step(self, train_batch, batch_idx):
+        """
+        Activate the training loop for the pl.LightningModule.
+
+        Override this method for implementing a new algorithm.
+        """
         raise NotImplementedError
-    
+
     def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        """
+        Activate the validation loop for the pl.LightningModule.
+
+        """
+
         if isinstance(batch[0], list):
             x = torch.cat([x for x, y, _ in batch])
             y = torch.cat([y for x, y, _ in batch])
         else:
             x = batch[0]
             y = batch[1]
-        
+
         out = self.model(x)
         loss = F.cross_entropy(out, y)
         acc = (torch.argmax(out, dim=1) == y).float().mean()
@@ -40,13 +59,20 @@ class PredictionAlgorithm(pl.LightningModule):
         self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True)
 
     def test_step(self, batch, batch_idx, dataloader_idx=0):
+        """
+        Activate the test loop for the pl.LightningModule.
+
+        Test loop is called only when `test()` is used.
+
+        """
+
         if isinstance(batch[0], list):
             x = torch.cat([x for x, y, _ in batch])
             y = torch.cat([y for x, y, _ in batch])
         else:
             x = batch[0]
             y = batch[1]
-        
+
         out = self.model(x)
         loss = F.cross_entropy(out, y)
         acc = (torch.argmax(out, dim=1) == y).float().mean()
@@ -55,6 +81,10 @@ class PredictionAlgorithm(pl.LightningModule):
         self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True)
 
     def configure_optimizers(self):
+        """
+        Initialize the optimizer using params passed when initializing PredictionAlgorithm class.
+
+        """
         if self.optimizer == "Adam":
             optimizer = torch.optim.Adam(
                 self.parameters(), lr=self.lr, weight_decay=self.weight_decay, betas=self.betas
@@ -63,5 +93,5 @@ class PredictionAlgorithm(pl.LightningModule):
             optimizer = torch.optim.SGD(
                 self.parameters(), lr=self.lr, weight_decay=self.weight_decay, momentum=self.momentum
             )
-        
+
         return optimizer
