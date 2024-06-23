c103b22b0bfcc4dde173acf6325716df9fec062a
andyjsw <91446731+andyjsw@users.noreply.github.com>
2024-06-23 13:05:15 +0700

    Changes to be committed: 	modified:   dowhy/causal_prediction_selfcode/algorithms/cacm.py

--- dowhy/causal_prediction_selfcode/algorithms/cacm.py
+++ dowhy/causal_prediction_selfcode/algorithms/cacm.py
@@ -1,6 +1,8 @@
 import torch
 from torch.nn import functional as F
 
+import numpy as np
+
 from dowhy.causal_prediction_selfcode.algorithms.base_algorithm import PredictionAlgorithm
 from dowhy.causal_prediction_selfcode.algorithms.regularization import Regularizer
 
@@ -10,6 +12,7 @@ class CACM(PredictionAlgorithm):
         self,
         model,
         sequence_classification=False,
+        n_groups_per_batch=1,
         optimizer="Adam",
         lr=1e-3,
         weight_decay=0.0,
@@ -39,6 +42,7 @@ class CACM(PredictionAlgorithm):
         self.lambda_sel = lambda_sel
 
         self.sequence_classification = sequence_classification
+        self.n_groups_per_batch = n_groups_per_batch
 
     def training_step(self, train_batch, batch_idx):
 
@@ -69,15 +73,45 @@ class CACM(PredictionAlgorithm):
         else:
             self.classifier = self.model
 
-            features = train_batch[0]
-            targets = train_batch[1]
+            nmb = self.n_groups_per_batch
+            batch_size = len(train_batch[1])
+            mb_size = int(batch_size/nmb)
+            
+            if torch.cuda.is_available():
+                device = 'cuda'
+            else:
+                device = 'cpu'
 
-            nmb = len(targets)
+            features = []
+            targets = []
+            attributes = []
+            classifs = []
+            # classifs = torch.empty(0, device=device, dtype=train_batch[1].dtype)
 
-            classifs = self.classifier(features)
+            for i in range(0, batch_size, mb_size):
+                mb_features = torch.empty((0, 300, 2), device=device, dtype=torch.long)
+                mb_targets = torch.empty(0, device=device, dtype=train_batch[1].dtype)
+                mb_attributes = torch.empty((0, 11), device=device, dtype=torch.long)
+                for j in range(i, i+mb_size):
+                    mb_features = torch.cat((mb_features, train_batch[0][j].unsqueeze(0)), dim=0)
+                    mb_targets = torch.cat((mb_targets, train_batch[1][j].unsqueeze(0)), dim=0)
+                    mb_attributes = torch.cat((mb_attributes, train_batch[2][j].unsqueeze(0)), dim=0)
+                features.append(mb_features)
+                targets.append(mb_targets)
+                attributes.append(mb_attributes)
+                print('self.classif')
+                print(self.classifier(mb_features))
+                classifs.append(self.classifier(mb_features))
+                # classifs = torch.cat((classifs, self.classifier(mb_features)), dim=0)
 
-            objective += F.cross_entropy(classifs, targets)
-            acc = (torch.argmax(classifs, dim=1) == targets).float().mean()
+            for i in range(nmb):
+                objective += F.cross_entropy(classifs[i], targets[i])
+                correct += (torch.argmax(classifs[i], dim=1) == targets[i]).float().sum().item()
+                total += classifs[i].shape[0]
+
+            acc = correct / total
+
+            print('acc', acc)
 
         objective /= nmb
         loss = objective
@@ -89,7 +123,7 @@ class CACM(PredictionAlgorithm):
                         ai for _, _, ai in minibatches
                     ]
                 else:
-                    attribute_labels = minibatches[2]
+                    attribute_labels = attributes
 
                 E_eq_A_attr = attr_type_idx in self.E_eq_A
 
